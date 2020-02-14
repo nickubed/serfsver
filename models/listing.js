24930848ac70ca890342ab4c204e771589f14d64
nickubed <nhpquandt@gmail.com>
2020-02-14 14:00:45 -0800

    added rat placeholder

--- models/listing.js
+++ models/listing.js
@@ -29,6 +29,12 @@ const listingSchema = new mongoose.Schema({
 listingSchema.pre('save', function(next, docs) {
     this.category = this.category.toLowerCase()
     this.subCategory = this.subCategory.toLowerCase()
+    if(this.img){
+        this.img = this.img
+    }
+    else{
+        this.img = 'https://imgprx.livejournal.net/18f7241a35f2cc0fc1302eb0a22eab234c3550f3/0uHGJXwL6JHiMcdx7HW7LPwOPsmxHNeLbVXtGMkSPToFVVi6JuK97BBa-WgPpNZ_whwd8NTujCtBAUNk_OFPD1Y7t23jyeBwGmRGpGLAlADk6HUbvQqyrgkkYpN1qtGNka5K6D0rYdnZLMA7UqIPow'
+    }
     next()
 })
 
