00379f126dc8441129f67e0e0e553c8b03d3b5a7
nickubed <nhpquandt@gmail.com>
2020-02-14 10:27:13 -0800

    added cat, subcat routes

--- controllers/listings.js
+++ controllers/listings.js
@@ -10,6 +10,28 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:category', (req, res) => {
+    db.listing.find({category: req.params.category})
+    .then(listings => {
+        res.send(listings)
+    })
+    .catch(err => {
+        console.log(err)
+        res.send(err)
+    })
+})
+
+router.get('/:subcategory', (req, res) => {
+    db.listing.find({subcategory: req.params.subcategory})
+    .then(listings => {
+        res.send(listings)
+    })
+    .catch(err => {
+        console.log(err)
+        res.send(err)
+    })
+})
+
 router.post('/new', (req, res) => {
     console.log('Hitting Post')
     db.listing.create(req.body)
